dd7613d4dd59d37cf37ae8c5a1872e66032908ae
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-09-26 13:06:21 -0500

    Add starter code main.rs

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,40 @@
+use yew::prelude::*;
+
+enum Msg { }
+
+struct Model {
+    // "ComponentLink is like a reference to a component"
+    _link: ComponentLink<Self>,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {
+            _link,
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _prop: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new porperties are different to previously recieved properties.
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <h1>{ "Hello, World!" }</h1>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
