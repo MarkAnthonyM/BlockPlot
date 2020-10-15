1721968d62bbb33712f077ffe8f60e63c3fe22ed
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-15 12:26:37 -0500

    Import base libraries

--- backend/src/bin/backend.rs
+++ backend/src/bin/backend.rs
@@ -1,3 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::databases::diesel;
+use rocket_contrib::json::Json;
+
+use rusty_rescuetime;
+
 fn main() {
     println!("Hello, World!");
 }
