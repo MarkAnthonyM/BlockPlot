c8376dfd297fbfde9303c40dda08e0bc1659139c
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-18 17:57:47 -0500

    Add TimeSheet struct to types.rs

--- frontend/src/types.rs
+++ frontend/src/types.rs
@@ -1,1 +1,8 @@
 use serde::{ Deserialize, Serialize };
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct TimeSheet {
+    pub notes: String,
+    row_headers: Vec<String>,
+    rows: Vec<String>,
+}
