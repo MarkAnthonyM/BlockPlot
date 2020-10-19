f7a024a4a8f06a73eb0c5a2b2054cbdb25d7b0f4
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-19 14:44:04 -0500

    Declare FetchResponse type alias

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -3,3 +3,5 @@ use anyhow::Error;
 use yew::callback::Callback;
 use yew::format::{ Json, Nothing };
 use yew::services::fetch::{ FetchService, FetchTask, Request, Response };
+
+pub type FetchResponse<T> = Response<Json<Result<T, Error>>>;
