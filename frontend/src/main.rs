fd1ce45ab2316bc1e89928ff3a80f714bd0e1430
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-19 14:26:20 -0500

    Add GetTimesheetsError variant to Msg enum

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -15,6 +15,7 @@ use yew::services::fetch::{ FetchService, FetchTask, Request, Response };
 enum Msg {
     GetTimeSheets,
     GetTimeSheetsSuccess(Vec<AnalyticData>),
+    GetTimesheetsError(Error),
 }
 
 struct Model {
