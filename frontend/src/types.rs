98926d9fa20e0dfa61b6a3e1deab8900b5aa7000
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-19 14:30:11 -0500

    Add QueryKind enum to types module

--- frontend/src/types.rs
+++ frontend/src/types.rs
@@ -7,3 +7,17 @@ pub struct AnalyticData {
     rows: Vec<QueryKind>,
 
 }
+
+//TODO: Current method of deserialization feels too messy. Try to find A more concise way to work with json data
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+enum QueryKind {
+    SizeFourInt(SizeFour<i32, i32>),
+    SizeFourMixedInt(SizeFour<i32, String>),
+    SizeFourMixedString(SizeFour<String, i32>),
+    SizeFourString(SizeFour<String, String>),
+    SizeSevenInt(SizeSeven<i32>),
+    SizeSevenString(SizeSeven<String>),
+    SizeSixInt(SizeSix<i32>),
+    SizeSixString(SizeSix<String>),
+}
