13149cd3f1409e6fbca510a6d4c07455f8bd0e29
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-23 23:45:18 -0500

    Add todo to types.rs

--- frontend/src/types.rs
+++ frontend/src/types.rs
@@ -1,5 +1,7 @@
 use serde::{ Deserialize, Serialize };
 
+//TODO: Currently declaring structs for json data deserialization twice.
+// Should used data types from Rusty-RescueTime when possible  
 #[derive(Debug, Deserialize, Serialize)]
 pub struct AnalyticData {
     pub notes: String,
