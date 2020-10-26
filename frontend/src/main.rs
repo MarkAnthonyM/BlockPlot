96162c530cbe1e25e31dc91462d4c145ff4ef86d
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-25 23:52:12 -0500

    Add todo

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -51,6 +51,7 @@ impl Model {
             // Loop for everyday in one week
             for y in 0..7 {
                 // Create <rect> element representing a day
+                //TODO: Add data-date attribute
                 let element = html! {
                     <rect width="11" height="11" y=y * 15 rx=2 ry=2 fill="#dadada" style="outline: 1px solid #1b1f230a; outline-offset: -1px;"></rect>
                 };
