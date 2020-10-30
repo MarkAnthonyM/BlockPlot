47dbea6c1a9a4f7915041247fdfb42516c0214b1
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-10-30 15:37:37 -0500

    Make TimeData struct fields public

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -2,6 +2,6 @@ use std::collections::HashMap;
 
 #[derive(Deserialize, Serialize)]
 pub struct TimeData {
-    category: String,
-    time_data: HashMap<String, u64>,
+    pub category: String,
+    pub time_data: HashMap<String, u64>,
 }
