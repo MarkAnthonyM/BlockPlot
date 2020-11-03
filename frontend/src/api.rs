612faa85bb93b2ff0d2d0eb9a4261b6097a71d8b
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-02 22:19:13 -0600

    Refactor get_dev_skillblocks method to use current timestamps

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -1,5 +1,7 @@
 use crate::types::TimeData;
 use anyhow::Error;
+use chrono::prelude::*;
+use yew::services::console::ConsoleService;
 use yew::callback::Callback;
 use yew::format::{ Json, Nothing };
 use yew::services::fetch::{ FetchService, FetchTask, Request, Response };
@@ -16,7 +18,12 @@ pub fn get_skillblocks(callback: FetchCallback<TimeData>) -> FetchTask {
 }
 
 pub fn get_dev_skillblocks(callback: FetchCallback<TimeData>) -> FetchTask {
-    let request = Request::get("http://localhost:8000/api/v1/categories/software_development")
+    let category = "software_development";
+    let end_date = Utc::now().date().naive_utc();
+    let begin_date = NaiveDate::from_ymd(end_date.year() - 1, end_date.month(), end_date.day() + 1);
+    let formatted_url = format!("http://localhost:8000/api/categories/{}?begin_date={}&end_date={}", category, begin_date, end_date);
+    ConsoleService::log(&formatted_url);
+    let request = Request::get(formatted_url)
         .body(Nothing)
         .unwrap();
 
