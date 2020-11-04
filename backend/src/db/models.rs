40de5504c3c775ca64a2d8e629b92782eb48fd03
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-04 02:04:36 -0600

    Add prototype TimeWrapper struct

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -5,3 +5,9 @@ pub struct TimeData {
     pub category: String,
     pub time_data: HashMap<String, i32>,
 }
+
+// Prototype wrapper struct for storing multiple TimeData requests
+#[derive(Deserialize, Serialize)]
+pub struct TimeWrapper {
+    pub data: Vec<TimeData>,
+}
