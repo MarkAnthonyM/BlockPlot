874a3a6733a50bd9582ebbafe2c892e229b71ec5
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-05 14:48:09 -0600

    Implement basic test logic in create.rs

--- frontend/src/pages/create.rs
+++ frontend/src/pages/create.rs
@@ -1,1 +1,71 @@
-//TODO: Build out logic to allow for creation of new skillblocks
+struct Form {
+    state: State,
+    link: ComponentLink<Self>,
+    task: Option<FetchTask>,
+}
+
+pub enum Msg {
+    PostData,
+    PostDataSuccess,
+    PostDataError(Error),
+}
+
+struct State {
+    form_data: FormData,
+    post_form_error: Option<Error>,
+    post_form_loaded: bool,
+}
+
+impl Component for Form {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        let mock_form_data = FormData {
+            name: "Test skill",
+            description: "This is a test description",
+        };
+        
+        Self {
+            state: State {
+                form_data: mock_form_data,
+                post_form_error: None,
+                post_form_success: false,
+            },
+            _link,
+            task: None,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::PostData => {
+                println!("Post some data!");
+
+                true
+            },
+            Msg::PostDataError => {
+                println!("Error posting data!");
+
+                true
+            },
+            Msg::PostDataSuccess => {
+                println!("Success posting data!");
+
+                true
+            },
+        }
+    }
+
+    fn change(&mut self, _prop: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <h1>
+                <p>{ "Hello, World!" }</p>
+            </h1>
+        }
+    }
+}
