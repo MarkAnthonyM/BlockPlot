114b6a4313b6c33ff96cf74ce9fb4b9b148c1057
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-12 10:27:03 -0600

    Fix type in skillblock table generation logic

--- migrations/2020-11-12-161951_create_skillblocks/up.sql
+++ migrations/2020-11-12-161951_create_skillblocks/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE skillblocks (
     id SERIAL PRIMARY KEY,
     category VARCHAR NOT NULL,
     skill_description TEXT NOT NULL,
-    skill_name TEXT NOT NULL,
+    skill_name TEXT NOT NULL
 )
