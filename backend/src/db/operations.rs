86eb6608766ac2940a75543ac9b4512b5b0d32ab
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-15 19:34:36 -0600

    Implement query_skillblock function in operations.rs

--- backend/src/db/operations.rs
+++ backend/src/db/operations.rs
@@ -8,3 +8,8 @@ pub fn create_skillblock(connection: &PgConnection, db_struct: models::NewSkillb
         .execute(connection)
         .expect("Error inserting into database");
 }
+
+// Query skillblock record from database
+pub fn query_skillblock(connection: &PgConnection) -> Vec<models::Skillblock> {
+    schema::skillblocks::table.load::<models::Skillblock>(connection).expect("Error querying record")
+}
