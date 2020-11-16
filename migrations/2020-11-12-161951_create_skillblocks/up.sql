338ec11f7101e48831f57248e5bdd9d5319ff975
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-15 19:33:27 -0600

    Update database table to contain user column

--- migrations/2020-11-12-161951_create_skillblocks/up.sql
+++ migrations/2020-11-12-161951_create_skillblocks/up.sql
@@ -2,5 +2,6 @@ CREATE TABLE skillblocks (
     id SERIAL PRIMARY KEY,
     category VARCHAR NOT NULL,
     skill_description TEXT NOT NULL,
-    skill_name TEXT NOT NULL
+    skill_name TEXT NOT NULL,
+    user TEXT NOT NULL
 )
