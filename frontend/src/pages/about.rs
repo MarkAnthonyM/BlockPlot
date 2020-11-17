d9feafe118fa454884972aa18be49bd19f769866
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-17 16:14:16 -0600

    Implement base logic in about.rs

---
+++ frontend/src/pages/about.rs
@@ -0,0 +1,30 @@
+use ybc::Container;
+
+use yew::prelude::*;
+
+pub struct About {}
+
+impl Component for About {
+    type Message = ();
+    type Properties = ();
+    
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <Container>
+                <h1 class="title is-1">{ "Coming Soon!" }</h1>
+            </Container>
+        }
+    }
+}
