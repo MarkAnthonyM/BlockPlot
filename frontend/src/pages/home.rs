9fde50e5656885cb4165040fe387bd1030098e50
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-17 15:52:01 -0600

    Add base logic to home.rs

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -1,3 +1,26 @@
 use yew::prelude::*;
 
 pub struct Home {}
+
+impl Component for Home {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <h1>{ "Hello, World!" }</h1>
+        }
+    }
+}
