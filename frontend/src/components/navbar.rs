bf7479f2546bd599a00ef6ab804a00f53e6456f0
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-17 04:25:21 -0600

    Implement base navbar logic

--- frontend/src/components/navbar.rs
+++ frontend/src/components/navbar.rs
@@ -1,1 +1,35 @@
+use ybc::Section;
+
 use yew::prelude::*;
+
+pub struct Navbar {}
+
+impl Component for Navbar {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <>
+                <div class="container">
+                    { self.view_navbar() }
+                </div>
+                <Section>
+                    { self.view_skill_blocks() }
+                </Section>
+            </>
+        }
+    }
+}
