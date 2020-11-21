933d1a7334705ab886081ae2ba696ef194b14240
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-20 23:32:19 -0600

    Declare sign_up/sign_in as modules in mod.rs

--- frontend/src/pages/mod.rs
+++ frontend/src/pages/mod.rs
@@ -1,9 +1,13 @@
 mod about;
 mod form;
 mod home;
+mod sign_in;
+mod sign_up;
 mod user;
 
 pub use about::About;
 pub use form::Form;
 pub use home::Home;
+pub use sign_in::SignIn;
+pub use sign_up::SignUp;
 pub use user::User;
