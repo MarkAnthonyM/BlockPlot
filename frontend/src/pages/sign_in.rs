b42236bc0447f870d7f47ffd18758fe28257e5d2
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-20 23:35:50 -0600

    Implement base logic for SignIn struct

---
+++ frontend/src/pages/sign_in.rs
@@ -0,0 +1,26 @@
+use yew::prelude::*;
+
+pub struct SignIn {}
+
+impl Component for SignIn {
+    type Message = ();
+    type Properties = ();
+    
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            
+        }
+    }
+}
