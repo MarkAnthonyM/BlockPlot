d8311ba4545e83e013602b26b1eefed619527631
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-11-23 04:52:52 -0600

    Implement prototype AuthParameters struct and logic

---
+++ backend/src/auth/auth0.rs
@@ -0,0 +1,34 @@
+use rocket::config::{ Config, ConfigError };
+
+// Store various parameters needed to build authorization link
+// that fetches auth0 login page. Parameters are read from
+// Rocket.toml configuration file
+pub struct AuthParameters {
+    pub audience: String,
+    pub auth0_domain: String,
+    pub client_id: String,
+    pub redirect_url: String,
+}
+
+impl AuthParameters {
+    pub fn new(config: &Config) -> Result<AuthParameters, ConfigError> {
+        let auth_parameters = Self {
+            audience: String::from(config.get_str("audience")?),
+            auth0_domain: String::from(config.get_str("auth0_domain")?),
+            client_id: String::from(config.get_str("client_id")?),
+            redirect_url: String::from(config.get_str("redirect_url")?),
+        };
+
+        Ok(auth_parameters)
+    }
+    
+    pub fn build_authorize_url(&self) -> String {
+        format!(
+            "https://{}/authorize?audience={}&response_type=token&client_id={}&redirect_uri={}",
+            self.auth0_domain,
+            self.audience,
+            self.client_id,
+            self.redirect_url
+        )
+    }
+}
