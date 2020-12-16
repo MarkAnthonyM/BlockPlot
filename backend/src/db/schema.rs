3c597708918d1d099dad5b319c24df6252e32ff9
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-12-16 13:12:17 -0600

    Create users db table migration

--- backend/src/db/schema.rs
+++ backend/src/db/schema.rs
@@ -8,3 +8,17 @@ table! {
         skill_description -> Text,
     }
 }
+
+table! {
+    users (auth_user) {
+        auth_user -> Text,
+        api_key -> Nullable<Text>,
+        key_stored -> Bool,
+        block_count -> Int4,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    skillblocks,
+    users,
+);
