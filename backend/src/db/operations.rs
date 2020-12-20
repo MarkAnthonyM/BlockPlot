04e4579de1e7b6ffef0b485ec15632bec46d5a35
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-12-20 05:22:00 -0600

    Add create_user db operation function

--- backend/src/db/operations.rs
+++ backend/src/db/operations.rs
@@ -12,6 +12,16 @@ pub fn create_skillblock(connection: &PgConnection, db_struct: models::NewSkillb
         .expect("Error inserting into database");
 }
 
+// Insert user record into database
+pub fn create_user(connection: &PgConnection, new_user: models::NewUser) -> Result<models::User, diesel::result::Error> {
+    let inserted_user = diesel::insert_into(schema::users::table)
+        .values(&new_user)
+        .get_result(connection)
+        .expect("Error inserting user into database");
+
+    Ok(inserted_user)
+}
+
 // Query skillblock record from database
 pub fn query_skillblock(connection: &PgConnection) -> Vec<models::Skillblock> {
     schema::skillblocks::table.load::<models::Skillblock>(connection).expect("Error querying record")
