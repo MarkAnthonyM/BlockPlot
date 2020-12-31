72f6b61ca6e0dfd4833510196906a2b4c97043a7
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-12-31 00:32:06 -0600

    Implement logic for frontend session state initialization

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,22 +1,78 @@
-use yew::prelude::*;
-use yew_router::prelude::*;
+use anyhow::Error;
 
+use crate::api;
 use crate::components::NavbarElement;
 use crate::pages::{ About, Form, Home, SignIn, SignUp, User };
 use crate::route::Route;
+use crate::types::Session;
 
-pub struct App {}
+use yew::format::Json;
+use yew::prelude::*;
+use yew_router::prelude::*;
+use yew::services::fetch::FetchTask;
+
+
+pub enum Msg {
+    CheckLoginStatus,
+    GetSessionError(Error),
+    GetSessionSuccess(Session),
+}
+
+pub struct App {
+    link: ComponentLink<Self>,
+    state: State,
+    task: Option<FetchTask>,
+}
+
+pub struct State {
+    session: Option<Session>,
+    session_checked: bool,
+}
 
 impl Component for App {
-    type Message = ();
+    type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        link.send_message(Msg::CheckLoginStatus);
+        
+        Self {
+            link,
+            state: State {
+                session: None,
+                session_checked: false,
+            },
+            task: None,
+        }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        true
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::CheckLoginStatus => {
+                self.state.session_checked = false;
+                let handler =
+                    self.link
+                        .callback(move |response: api::FetchResponse<Session>| {
+                            let (_, Json(data)) = response.into_parts();
+                            match data {
+                                Ok(session_info) => Msg::GetSessionSuccess(session_info),
+                                Err(error) => Msg::GetSessionError(error),
+                            }
+                        });
+                self.task = Some(api::get_user_session(handler));
+                true
+            },
+            Msg::GetSessionError(_error) => {
+                self.state.session = None;
+                self.state.session_checked = true;
+                true
+            },
+            Msg::GetSessionSuccess(session) => {
+                self.state.session = Some(session);
+                self.state.session_checked = true;
+                true
+            }
+        }
     }
 
     fn change(&mut self, _: Self::Properties) -> ShouldRender {
