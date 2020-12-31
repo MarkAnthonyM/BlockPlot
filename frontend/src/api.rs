327a8a66e7e53425d00414ef680907bbee0cd5b5
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2020-12-31 00:12:33 -0600

    Add get_user_session function in api module

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -1,5 +1,5 @@
 use anyhow::Error;
-use crate::types::TimeWrapper;
+use crate::types::{ Session, TimeWrapper};
 use yew::callback::Callback;
 use yew::format::{ Json, Nothing };
 use yew::services::fetch::{ FetchOptions, FetchService, FetchTask, Request, Response };
@@ -11,6 +11,20 @@ type FetchCallback<T> = Callback<FetchResponse<T>>;
 // A testing request getter. Will probably remove at conclusion of tests
 pub fn get_dev_skillblocks(callback: FetchCallback<TimeWrapper>) -> FetchTask {
     let url = format!("http://localhost:8000/api/skillblocks");
+    let request = Request::get(url)
+        .body(Nothing)
+        .unwrap();
+    let options = FetchOptions {
+        credentials: Some(RequestCredentials::Include),
+        ..FetchOptions::default()
+    };
+
+    FetchService::fetch_binary_with_options(request, options, callback).unwrap()
+}
+
+// Fetch user session information
+pub fn get_user_session(callback: FetchCallback<Session>) -> FetchTask {
+    let url = format!("http://localhost:8000/home");
     let request = Request::get(url)
         .body(Nothing)
         .unwrap();
