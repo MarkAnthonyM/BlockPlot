63a3f8386d4a943eb9fab8435a299c60c885e610
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-01-05 15:47:25 -0600

    Update navbar logic to leverage session state

--- frontend/src/components/navbar.rs
+++ frontend/src/components/navbar.rs
@@ -1,4 +1,5 @@
 use crate::route::Route;
+use crate::types::Session;
 
 use ybc::{ Container, Navbar };
 
@@ -6,7 +7,15 @@ use yew::prelude::*;
 
 use yew_router::components::RouterAnchor;
 
-pub struct NavbarElement {}
+pub struct NavbarElement {
+    props: Props,
+}
+
+#[derive(Properties, Clone)]
+pub struct Props {
+    #[prop_or_default]
+    pub session: Option<Session>,
+}
 
 impl NavbarElement {
     // Contruct navbar at top of page
@@ -32,37 +41,84 @@ impl NavbarElement {
 
     // Construct navend section of navbar
     fn view_navend(&self) -> Html {
-        html! {
-            <div class="navbar-end">
-                <div class="navbar-item">
-                    <div class="buttons">
+        let login_signup_ui = match self.props.session.as_ref() {
+            Some(session) => {
+                html! {
+                    <a href="http://localhost:8000/logout" class="button is-light">
+                        { "Log Out" }
+                    </a>
+                }
+            },
+            None => {
+                html! {
+                    <>
                         <a href="http://localhost:8000/auth0" class="button is-primary">
                             <strong>{ "Sign Up" }</strong>
                         </a>
                         <a href="http://localhost:8000/auth0" class="button is-light">
                             { "Log In" }
                         </a>
-                        <a href="http://localhost:8000/logout" class="button is-light">
-                            { "Log Out" }
-                        </a>
+                    </>
+                }
+            }
+        };
+        let picture_div = match self.props.session.as_ref() {
+            Some(session) => {
+                html! {
+                    <figure class="image is-96x96">
+                        <img class="is-rounded" src={ session.picture.as_str() }/>
+                    </figure>
+                }
+            },
+            None => {
+                html! {
+                    <>
+                    </>
+                }
+            }
+        };
+
+        html! {
+            <>
+                <div class="navbar-item">
+                    <div class="buttons">
+                        { login_signup_ui }
                     </div>
                 </div>
-            </div>
+                <div style="padding-top:0.5rem">
+                    { picture_div }
+                </div>
+            </>
         }
     }
 
     // Construct main section of navbar
     fn view_navstart(&self) -> Html {
         type Anchor = RouterAnchor<Route>;
+        let user_div = match self.props.session.as_ref() {
+            Some(session) => {
+                html! {
+                    <>
+                        <Anchor route=Route::UserPage classes="navbar-item">
+                            <p class="is-size-4">{ format!("Hi, {}!", &session.given_name) }</p>
+                        </Anchor>
+                        <Anchor route=Route::FormPage classes="navbar-item">
+                            <p class="is-size-4">{ "Create" }</p>
+                        </Anchor>
+                    </>
+                }
+            },
+            None => {
+                html! {
+                    <p class="is-size-4 navbar-item">{ "Welcome!" }</p>
+                }
+            }
+        };
         
         html! {
             <>
-                <Anchor route=Route::UserPage classes="navbar-item">
-                    <p class="is-size-4">{ "UserName" }</p>
-                </Anchor>
-                <Anchor route=Route::FormPage classes="navbar-item">
-                    <p class="is-size-4">{ "Create" }</p>
-                </Anchor>
+                { user_div }
+                <p class="is-size-4 navbar-item">{ "Explore" }</p>
                 <Anchor route=Route::AboutPage classes="navbar-item">
                     <p class="is-size-4">{ "About" }</p>
                 </Anchor>
@@ -73,18 +129,19 @@ impl NavbarElement {
 
 impl Component for NavbarElement {
     type Message = ();
-    type Properties = ();
+    type Properties = Props;
 
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        Self {}
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self { props }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
         true
     }
 
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.props = props;
+        true
     }
 
     fn view(&self) -> Html {
