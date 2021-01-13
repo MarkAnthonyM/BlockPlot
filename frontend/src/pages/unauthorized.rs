ffe0403c2e1e99de3efc5806d2bdf37b531c2941
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-01-13 12:48:37 -0600

    Add unauthorized page

---
+++ frontend/src/pages/unauthorized.rs
@@ -0,0 +1,26 @@
+use yew::prelude::*;
+
+pub struct Unauthorized {}
+
+impl Component for Unauthorized {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p>{ "This is a test!" }</p>
+        }
+    }
+}
