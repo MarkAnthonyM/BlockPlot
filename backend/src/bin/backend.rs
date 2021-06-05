9b36124e1fbaf1c5ba1132c41f435e354f89e6fe
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-06-05 04:34:42 -0500

    Refactor rocket ignition logic

--- backend/src/bin/backend.rs
+++ backend/src/bin/backend.rs
@@ -1,45 +1,5 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+use backend;
 
-#[macro_use]
-extern crate rocket;
-
-use backend::auth::auth0::{AuthParameters, SessionDB};
-use backend::db::operations::BlockplotDbConn;
-
-use dashmap::DashMap;
-
-use rocket::fairing::AdHoc;
-
-use rocket_contrib::templates::Template;
-use rocket_cors::{ AllowedHeaders, AllowedOrigins, Error };
-
-fn main() -> Result<(), Error> {
-    let allowed_origins = AllowedOrigins::all();
-
-    let cors = rocket_cors::CorsOptions {
-        allowed_origins,
-        //TODO: Swtich to more strict options
-        allowed_headers: AllowedHeaders::all(),
-        allow_credentials: true,
-        ..Default::default()
-    }
-        .to_cors()?;
-    
-    let sessions = SessionDB(DashMap::new());
-    
-    rocket::ignite()
-        .attach(BlockplotDbConn::fairing())
-        .attach(cors)
-        .attach(Template::fairing())
-        .mount("/", routes![auth0_login, home, index, get_skillblocks, get_skillblocks_redirect, process_login, process_logout, test_post])
-        .manage(sessions)
-        .attach(AdHoc::on_attach("Parameters Config", |rocket| {
-            let config = rocket.config();
-            let auth_parameters = AuthParameters::new(config).unwrap();
-
-            Ok(rocket.manage(auth_parameters))
-        }))
-        .launch();
-
-    Ok(())
+fn main() {
+    backend::rocket().launch();
 }
--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -7,9 +7,58 @@ extern crate serde;
 #[macro_use]
 extern crate rocket;
 
+use crate::auth::auth0::{AuthParameters, SessionDB};
+use crate::db::operations::BlockplotDbConn;
+
+use dashmap::DashMap;
+
+use rocket::fairing::AdHoc;
+use rocket_contrib::templates::Template;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
+
 pub mod auth;
 pub mod db;
 pub mod routes;
+
+pub fn rocket() -> rocket::Rocket {
+    let allowed_origins = AllowedOrigins::all();
+
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        //TODO: Swtich to more strict options
+        allowed_headers: AllowedHeaders::all(),
+        allow_credentials: true,
+        ..Default::default()
+    }
+        .to_cors();
+    
+    let sessions = SessionDB(DashMap::new());
+    
+    rocket::ignite()
+        .attach(BlockplotDbConn::fairing())
+        .attach(cors.unwrap())
+        .attach(Template::fairing())
+        .mount(
+            "/",
+            routes![
+                routes::authentication::auth0_login,
+                routes::authentication::process_login,
+                routes::authentication::process_logout,
+                routes::index::home,
+                routes::index::index,
+                routes::skillblocks::get_skillblocks,
+                routes::skillblocks::get_skillblocks_redirect,
+                routes::skillblocks::test_post,
+            ],
+        )
+        .manage(sessions)
+        .attach(AdHoc::on_attach("Parameters Config", |rocket| {
+            let config = rocket.config();
+            let auth_parameters = AuthParameters::new(config).unwrap();
+
+            Ok(rocket.manage(auth_parameters))
+        }))
+}
 
 #[cfg(test)]
 mod tests {
