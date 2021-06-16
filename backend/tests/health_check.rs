fd58b4a80713cd291a7da2ad7d5a4ee1996dea62
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-06-16 05:53:14 -0500

    Adjust delay interval between selenium testing attemps

--- backend/tests/health_check.rs
+++ backend/tests/health_check.rs
@@ -116,7 +116,7 @@ fn configure_testuser(app: &TestApp) -> WebDriverResult<LocalResponse> {
     // Delays necessary to give webpage dom elements enough time
     // to load up properly
     driver.get(&auth_uri)?;
-    let delay = Duration::new(3, 0);
+    let delay = Duration::new(2, 0);
     sleep(delay);
 
     // Crawl to google sign-in button element
